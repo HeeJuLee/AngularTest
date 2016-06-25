63aa329569d28a3823cc3c89005971711e7ac2f5
HeeJu Lee <leeheeju@gmail.com>
2016-06-25 12:15:58 +0900

    chapter2. Directives: ng-click, ng-init, ng-show, ng-hide, ng-if, ng-repeat, ng-class, ng-style, ng-cloak

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,33 @@
-function AppCtrl($scope) {
+var testApp = angular.module('testApp', []);
+
+testApp.controller('helloController', function testController($scope) {
 	$scope.name = "World";
-}
+})
+
+testApp.controller('contactController', function contactController($scope) {
+	$scope.clickHandler = function() {
+		$scope.isHidden = !$scope.isHidden;
+	};
+	
+	$scope.contacts = [
+		 {
+			 name: 'john',
+			 phone: '010 2344 2342',
+			 email: 'john@exampl.com'
+		 },
+		 {
+			 name: 'heelee',
+			 phone: '010 3405 4121',
+			 email: 'hjlee@exampl.com'
+		 }
+	];
+	
+	$scope.styleList = function() {
+		if(!$scope.styler)
+			return;
+		return {
+			background: 'red',
+			fontWeight: 'bold'
+		};
+	};
+})
